890045c77e5ee41c5f036f65100958f142251c11
nikk-16 <nikhilchaurasiya16401@gmail.com>
2024-01-03 17:54:36 +0530

    new branch to get reviewed

--- backend/src/main/java/com/claims/manage/repositories/ClaimsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.claims.manage.repositories;
-
-import com.claims.manage.models.Claims;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClaimsRepository extends MongoRepository<Claims, String> {
-}
