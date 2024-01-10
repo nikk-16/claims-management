d26fc13f41e73be688c416018b59621955f710ab
Desu Sri Meghana <desu.meghana@gmail.com>
2024-01-10 10:55:22 +0530

    commit

--- frontend/src/shared/shared-components/insurance-description-page/insurance-description-page.component.ts
+++ frontend/src/shared/shared-components/insurance-description-page/insurance-description-page.component.ts
@@ -9,10 +9,9 @@ import { InsurancePoliciesService } from 'src/app/services/insurance-policies.se
   styleUrl: './insurance-description-page.component.scss'
 })
 export class InsuranceDescriptionPageComponent {
-  insuranceId!: string;
-  insurancePolicy!:InsurancePolicies;
-  username = localStorage.getItem("username")!;
-  
+  insuranceId: string|any;
+  insurancePolicy:InsurancePolicies|any;
+  username :string |any= localStorage.getItem("username");
   constructor(private ar: ActivatedRoute, private insurancePoliciesService:InsurancePoliciesService){
     this.insuranceId = ar.snapshot.params['id'];
   }
